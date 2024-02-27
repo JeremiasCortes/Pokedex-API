150a07ed7fc9455f58ca5743461c375a4d04a5a6
Jeremy <jeremiasacortes@gmail.com>
2024-02-27 21:23:59 +0100

    #

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Aside from './componentes/Aside'
 function App() {
   return (
     <section>
-      <main>
+      <main className='max-w-[1400px] mx-auto grid grid-cols-1 lg:grid-cols-2 h-screen'>
         <Pokemones></Pokemones>
         <Aside></Aside>
       </main>
